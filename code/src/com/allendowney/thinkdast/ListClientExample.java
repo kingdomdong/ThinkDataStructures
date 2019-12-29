1db56415b4b0fe194ffc40c9491d63dc32230124
847311033 <847311033@qq.com>
2019-12-29 19:47:09 +0800

    Think data structures   Chapter1 thinkdast

--- code/src/com/allendowney/thinkdast/ListClientExample.java
+++ code/src/com/allendowney/thinkdast/ListClientExample.java
@@ -1,26 +1,22 @@
 package com.allendowney.thinkdast;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListClientExample {
-	@SuppressWarnings("rawtypes")
-	private List list;
+    private List<Integer> list;
 
-	@SuppressWarnings("rawtypes")
 	public ListClientExample() {
-		list = new LinkedList();
-	}
+        list = new ArrayList();
+    }
 
-	@SuppressWarnings("rawtypes")
-	public List getList() {
+    public List<Integer> getList() {
 		return list;
 	}
 
 	public static void main(String[] args) {
-		ListClientExample lce = new ListClientExample();
-		@SuppressWarnings("rawtypes")
-		List list = lce.getList();
+        ListClientExample listClientExample = new ListClientExample();
+        List<Integer> list = listClientExample.getList();
 		System.out.println(list);
 	}
 }
